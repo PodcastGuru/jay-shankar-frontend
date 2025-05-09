9c19d465092ba6a78d8e9758f4127bdc8f3ac32b
Aniket Singh <aniketsingh@dshgsonic.com>
2025-05-09 17:07:24 +0530

    updated functionality of language

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -1,19 +1,3 @@
-// import Header from './Header';
-// import Footer from './Footer';
-// import '../app/globals.css';
-
-// export default function Layout({ children, title = 'MediCare Nursing' }) {
-//   return (
-//     <div className="font-geist-sans antialiased flex flex-col min-h-screen">
-//       <Header />
-//       <main className="flex-grow"> {/* Add pt-20 to push content below the fixed header */}
-//         {children}
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// }
-
 
 
 
